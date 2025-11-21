aecb2948c84ce99cbbb6304920a0be514910e369
haileylsn-oss <haileylsn@gmail.com>
2025-11-21 18:23:34 +0100

    kk

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import { FaFacebook, FaWhatsapp } from 'react-icons/fa';
 import ceo from '../assets/ceo.jpg'
+import ceo2 from '../assets/ceo2.jpg'
 import StatsSection from '../components/stat';
 import OurTeam from '../components/ourteam';
 
@@ -41,6 +42,36 @@ FOUNDER</h2>
       </a>
     
     </div>
+        </div>
+      </section>
+      {/* About & Image */}
+      <section className="max-w-6xl mx-auto px-6 py-12 grid md:grid-cols-2 gap-8 items-center">
+        {/* Image */}
+        <img
+          src={ceo2}
+          alt="CEO of Kids Empathy Initiative"
+          className="w-full rounded-lg shadow-lg"
+        />
+
+        {/* About Text */}
+        <div>
+          <p className='text-2xl font-semibold'>Akindehinde OLATUNBOSUN Afeez
+</p>
+          <h2 className="text-3xl font-thin mb-4">
+CO-FOUNDER</h2>
+
+         <p className="text-gray-300 mb-6"> Our work is rooted in a humanitarian mission to protect and support children in underserved communities. We focus on meeting essential needs, restoring stability, and nurturing their emotional and educational development. Through compassion-driven action, we aim to ensure that every child can experience safety, belonging, and possibility. </p>
+          {/* <div className="flex space-x-4 justify-center items-center mt-6">
+      <a href="https://www.facebook.com/share/1FYEnBpB6H/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer"
+         className="p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition duration-300">
+        <FaFacebook className="w-5 h-5" />
+      </a>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"
+         className="p-2 bg-green-400 hover:bg-green-500 text-white rounded-full transition duration-300">
+        <FaWhatsapp className="w-5 h-5" />
+      </a>
+    
+    </div> */}
         </div>
       </section>
       <section className="max-w-6xl mx-auto px-6 py-12 grid md:grid-cols-2 gap-8 items-center">
